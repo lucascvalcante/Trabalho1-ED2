e9a0f8e0041f82738331b18dee02e89f43a959ce
Lucas <lucaskcavalcante@gmail.com>
2026-03-24 22:11:51 -0300

    alteração na estrutura de pastas

--- tests/testes.c
+++ tests/testes.c
@@ -4,24 +4,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// --- ESTRUTURA FAKE PARA OS TESTES ---
 typedef struct {
     int valor;
 } Dado;
 
-// Variável global para ser usada nos testes
 exHash hash = NULL; 
 
-// --- SETUP E TEARDOWN EXIGIDOS PELO UNITY ---
-// Roda ANTES de cada teste
 void setUp(void) {
     remove("teste_hash.bin");
     remove("teste_hash.bin_dir.bin");
-    // Já cria o hash fresco para cada teste usar
     hash = init_exHash("teste_hash.bin", 2, sizeof(Dado)); 
 }
 
-// Roda DEPOIS de cada teste
 void tearDown(void) {
     if (hash != NULL) {
         close_exHash(hash);
@@ -31,30 +25,28 @@ void tearDown(void) {
     remove("teste_hash.bin_dir.bin");
 }
 
-// --- CASOS DE TESTE ---
 
-void test_InicializarHashDoZero_DeveRetornarPonteiroValido(void) {
+void test_InitHash(void) {
     TEST_ASSERT_NOT_NULL(hash);
 }
 
-void test_InserirElementos_E_ForcarSplit_DeveRetornarTrue(void) {
+void test_InsertElement(void) {
     Dado d1 = {100};
     Dado d2 = {200};
-    Dado d3 = {300}; // Força o split (tamanho do bucket é 2)
+    Dado d3 = {300}; 
 
     TEST_ASSERT_TRUE(insert_exHash(hash, 10, &d1));
     TEST_ASSERT_TRUE(insert_exHash(hash, 20, &d2));
     TEST_ASSERT_TRUE(insert_exHash(hash, 30, &d3));
 }
 
-void test_InserirChaveDuplicada_DeveRetornarFalse(void) {
+void test_InsertKey(void) {
     Dado d1 = {100};
     TEST_ASSERT_TRUE(insert_exHash(hash, 10, &d1));
-    // Tenta de novo com a mesma chave
     TEST_ASSERT_FALSE(insert_exHash(hash, 10, &d1)); 
 }
 
-void test_BuscarElementoExistente_DeveRetornarDadoCorreto(void) {
+void test_SearchExistingElements(void) {
     Dado d2 = {200};
     insert_exHash(hash, 20, &d2);
 
@@ -66,33 +58,29 @@ void test_BuscarElementoExistente_DeveRetornarDadoCorreto(void) {
     free(busca);
 }
 
-void test_BuscarElementoInexistente_DeveRetornarNull(void) {
-    // "Caminho Infeliz" pedido pelo professor
+void test_SearchUnexistingElements(void) {
     Dado* busca = (Dado*) search_exHash(hash, 99);
     TEST_ASSERT_NULL(busca);
 }
 
-void test_RemoverElementoExistente_DeveRetornarTrue(void) {
+void test_RemoveExistingElement(void) {
     Dado d1 = {100};
     insert_exHash(hash, 10, &d1);
 
     TEST_ASSERT_TRUE(remove_exHash(hash, 10));
-    
-    // Garante que sumiu mesmo
     Dado* busca = (Dado*) search_exHash(hash, 10);
     TEST_ASSERT_NULL(busca);
 }
 
-// --- MAIN DO UNITY ---
 int main(void) {
     UNITY_BEGIN();
     
-    RUN_TEST(test_InicializarHashDoZero_DeveRetornarPonteiroValido);
-    RUN_TEST(test_InserirElementos_E_ForcarSplit_DeveRetornarTrue);
-    RUN_TEST(test_InserirChaveDuplicada_DeveRetornarFalse);
-    RUN_TEST(test_BuscarElementoExistente_DeveRetornarDadoCorreto);
-    RUN_TEST(test_BuscarElementoInexistente_DeveRetornarNull);
-    RUN_TEST(test_RemoverElementoExistente_DeveRetornarTrue);
+    RUN_TEST(test_InitHash);
+    RUN_TEST(test_InsertElement);
+    RUN_TEST(test_InsertKey);
+    RUN_TEST(test_SearchExistingElements);
+    RUN_TEST(test_SearchUnexistingElements);
+    RUN_TEST(test_RemoveExistingElement);
     
     return UNITY_END();
 }

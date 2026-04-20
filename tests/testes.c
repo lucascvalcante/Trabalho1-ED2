3f16b943259d26cf4ef2a449a57237136d4d27a8
Lucas <lucaskcavalcante@gmail.com>
2026-04-20 20:54:01 -0300

    fix: alteração dos testes do hashfile

--- tests/testes.c
+++ tests/testes.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "exhash.h"
+#include "exHash.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,9 +11,10 @@ typedef struct {
 exHash hash = NULL; 
 
 void setUp(void) {
-    remove("teste_hash.bin");
-    remove("teste_hash.bin_dir.bin");
-    hash = init_exHash("teste_hash.bin", 2, sizeof(Dado)); 
+
+    remove("teste_hash.hf");
+    remove("teste_hash.dir");
+    hash = init_exHash("teste_hash.hf", 2, sizeof(Dado)); 
 }
 
 void tearDown(void) {
@@ -21,10 +22,11 @@ void tearDown(void) {
         close_exHash(hash);
         hash = NULL;
     }
-    remove("teste_hash.bin");
-    remove("teste_hash.bin_dir.bin");
-}
 
+    remove("teste_hash.hf");
+    remove("teste_hash.dir");
+    remove("teste_hash_dump.hfd");
+}
 
 void test_InitHash(void) {
     TEST_ASSERT_NOT_NULL(hash);
@@ -35,41 +37,56 @@ void test_InsertElement(void) {
     Dado d2 = {200};
     Dado d3 = {300}; 
 
-    TEST_ASSERT_TRUE(insert_exHash(hash, 10, &d1));
-    TEST_ASSERT_TRUE(insert_exHash(hash, 20, &d2));
-    TEST_ASSERT_TRUE(insert_exHash(hash, 30, &d3));
+    TEST_ASSERT_TRUE(insert_exHash(hash, "CEP10", &d1));
+    TEST_ASSERT_TRUE(insert_exHash(hash, "CEP20", &d2));
+    TEST_ASSERT_TRUE(insert_exHash(hash, "CEP30", &d3));
 }
 
-void test_InsertKey(void) {
+void test_InsertDuplicatedKey(void) {
     Dado d1 = {100};
-    TEST_ASSERT_TRUE(insert_exHash(hash, 10, &d1));
-    TEST_ASSERT_FALSE(insert_exHash(hash, 10, &d1)); 
+    TEST_ASSERT_TRUE(insert_exHash(hash, "CEP10", &d1));
+    TEST_ASSERT_FALSE(insert_exHash(hash, "CEP10", &d1)); 
 }
 
 void test_SearchExistingElements(void) {
     Dado d2 = {200};
-    insert_exHash(hash, 20, &d2);
+    insert_exHash(hash, "CEP20", &d2);
 
-    Dado* busca = (Dado*) search_exHash(hash, 20);
+    Dado* busca = (Dado*) search_exHash(hash, "CEP20");
     
     TEST_ASSERT_NOT_NULL(busca);
     TEST_ASSERT_EQUAL_INT(200, busca->valor);
     
-    free(busca);
+    free(busca); 
 }
 
 void test_SearchUnexistingElements(void) {
-    Dado* busca = (Dado*) search_exHash(hash, 99);
+    Dado* busca = (Dado*) search_exHash(hash, "NAO_EXISTE");
     TEST_ASSERT_NULL(busca);
 }
 
 void test_RemoveExistingElement(void) {
     Dado d1 = {100};
-    insert_exHash(hash, 10, &d1);
+    insert_exHash(hash, "CEP10", &d1);
 
-    TEST_ASSERT_TRUE(remove_exHash(hash, 10));
-    Dado* busca = (Dado*) search_exHash(hash, 10);
+    TEST_ASSERT_TRUE(remove_exHash(hash, "CEP10"));
+    Dado* busca = (Dado*) search_exHash(hash, "CEP10");
     TEST_ASSERT_NULL(busca);
+}
+
+void test_LoadHash(void) {
+    Dado d1 = {999};
+    insert_exHash(hash, "PERSISTE", &d1);
+    close_exHash(hash);
+    hash = NULL;
+    hash = load_exHash("teste_hash.hf");
+    TEST_ASSERT_NOT_NULL(hash);
+    
+    Dado* busca = (Dado*) search_exHash(hash, "PERSISTE");
+    TEST_ASSERT_NOT_NULL(busca);
+    TEST_ASSERT_EQUAL_INT(999, busca->valor);
+    
+    free(busca);
 }
 
 int main(void) {
@@ -77,10 +94,11 @@ int main(void) {
     
     RUN_TEST(test_InitHash);
     RUN_TEST(test_InsertElement);
-    RUN_TEST(test_InsertKey);
+    RUN_TEST(test_InsertDuplicatedKey);
     RUN_TEST(test_SearchExistingElements);
     RUN_TEST(test_SearchUnexistingElements);
     RUN_TEST(test_RemoveExistingElement);
+    RUN_TEST(test_LoadHash); 
     
     return UNITY_END();
 }
